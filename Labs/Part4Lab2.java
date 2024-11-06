f64bab401c67c836c221c6d9dc5b568f59180a56
ernie <zermenoernesto@yahoo.com>
2024-11-06 15:04:33 -0700

    Part4Lab3

--- Labs/Part4Lab2.java
+++ Labs/Part4Lab2.java
@@ -2,9 +2,9 @@ import java.lang.Math;
 public class Part4Lab2 {
     public static void main(String[] args) {
         double side1, side2;
-        side1 = 5;
+        side1 = 4;
         side2 = 3;
         double hypotenuse = Math.sqrt(side1 * side1 + side2 * side2);
-        System.out.println("The length of the hypotenuse is:" + hypotenuse);
+        System.out.println("The length of the hypotenuse is:" + hypotenuse//"for a right triangle with side lengths of 3.0 and 4.0.");
     }
 }
