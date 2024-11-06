a49a02c61a30c6053f78d031336007965595cb52
ernie <zermenoernesto@yahoo.com>
2024-11-06 13:41:00 -0700

    Part4Lab1

---
+++ Labs/Part4Lab1.java
@@ -0,0 +1,8 @@
+public class Part4Lab1 {
+    public static void main(String[] args) {
+        double num1, num2, num3, num4, num5;
+        double average = (1+2+3+4+5)/5;
+        System.out.println("the average of 1, 2, 3, 4, and 5 is:" + average);
+
+    }
+}
