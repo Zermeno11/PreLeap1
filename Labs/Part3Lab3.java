01924a9bb3f78306c674f1ad54860d3db2e91487
ernie <zermenoernesto@yahoo.com>
2024-11-06 12:57:14 -0700

    Part3Lab3

---
+++ Labs/Part3Lab3.java
@@ -0,0 +1,12 @@
+public class Part3Lab3 {
+    public static void main(String[] args) {
+  int age = 25;
+  Double weight = 160.5;
+  boolean statement = true;
+  String name = "Alice";
+    System.out.println("Age:" + age);
+    System.out.println("Weight:" + weight);
+    System.out.println("Is a Student:" + statement);
+    System.out.println("First Name:" + name);
+    }
+}
