e66e53aef59038f63f63571327ffa21422136706
ernie <zermenoernesto@yahoo.com>
2024-11-06 06:42:47 -0700

    Novemebr 6 2024

---
+++ Labs/Part3Lab2.java
@@ -0,0 +1,10 @@
+public class Part3Lab2 {
+    public static void main(String[] args) {
+        String name = "Ernesto Zermeno";
+        int age = 32;
+        String state = "Illinois";
+        System.out.println("My Name:" + name);
+        System.out.println("My age:" + age);
+        System.out.println("State of Birth:" + state);
+    }
+}
