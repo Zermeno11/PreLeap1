613a6c310e51bcdd343977a03cb1f704c5e64afe
ernie <zermenoernesto@yahoo.com>
2024-11-18 20:24:07 -0700

    Part6Lab1

---
+++ Labs/Part6Lab1.java
@@ -0,0 +1,14 @@
+public class Part6Lab1 {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 20;
+
+        if (number1 > number2) {
+            System.out.println("Number 1 is greater than Number2.");
+        } else if (number1 < number2) {
+            System.out.println("Number 1 is less than Number2.");
+        } else {  
+            System.out.println("Number 1 is equal to Number2."); 
+        }
+     }
+}
