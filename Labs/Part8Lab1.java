f25dade30928cf41ce418ce30f613398117c1ce6
ernie <zermenoernesto@yahoo.com>
2024-12-04 20:11:21 -0700

    Part8Lab1

---
+++ Labs/Part8Lab1.java
@@ -0,0 +1,10 @@
+public class Part8Lab1 {
+    public static void main(String[] args) {
+        int n = 20;
+        int i = 1;
+        while (i <= n) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
